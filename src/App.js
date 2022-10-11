bfdd2bb9c68511fe0ab401b9921c78bc0d499cf9
Jafrin <jafrin3347@gmail.com>
2022-10-11 14:20:56 +0600

    nav-title-favicon-1-done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Main from './layout/Main';
+import Blog from './components/Blog/Blog';
+import Statistics from './components/Statistics/Statistics';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        { path: '/', element: <Home></Home> },
+        { path: '/home', element: <Home></Home> },
+        { path: '/blog', element: <Blog></Blog> },
+        { path: '/statistics', element: <Statistics></Statistics> },
+      ]
+    },
+    { path: '/home', element: <Home></Home> },
+
+  ])
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container">
+                    <a className="navbar-brand" href="#">CS MCQ Factory</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link to='/home'>Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/blog'>Blog</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/statistics'>Statistics</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <p>Common Header</p>
+
+        </div>
+    );
+};
+
+export default Header;
